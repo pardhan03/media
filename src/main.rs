0866d46d6929051d02be9450f51c2d9db661298a
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-08-07 18:39:58 +0530

    making modular folder strucure

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,10 @@
-#[derive(Debug)]
-enum Media {
-    Book { title: String, author: String },
-    Movie { title: String, director: String },
-    Audiobook { title: String },
-    Podcast(u32),
-    Placeholder
-}
-
-impl Media {
-    fn description(&self) -> String {
-        // if let Media::Audiobook { title } = self {
-        //     format!("Audiobook: {}", title)
-        // } else if let Media::Book {title, author} = self {
-        //     format!("Book {}, {}", title, author)
-        // } else if let Media::Movie {title, director} = self {
-        //     format!("Movie: {}, {}", title, director)
-        // } else {
-        //     String::from("Media description")
-        // }
-
-        match self {
-            Media::Audiobook { title } => {
-                format!("Audiobook: {}", title)
-            }
-            Media::Book { title, author } => {
-                format!("Book {}, {}", title, author)
-            }
-            Media::Movie { title, director } => {
-                format!("Movie: {}, {}", title, director)
-            }
-            Media::Podcast(id) => {
-                format!("Podcast: {}", id)
-            }
-            Media::Placeholder => {
-                format!("Placeholder:")
-            }
-        }
-    }
-}
-
-#[derive(Debug)]
-struct  Catalog {
-    items: Vec<Media>
-}
-
-impl Catalog {
-    fn new() -> Self {
-        Catalog { items: vec![]}
-    }
-
-    fn add(&mut self, media: Media) {
-        self.items.push(media);
-    }
-
-    fn get_by_index(&self, index: usize) -> Option<&Media> {
-        // &self.items[index]
-        if self.items.len() > index {
-            Some(&self.items[index])
-        } else {
-            None
-        }
-    }
-}
-fn print_media(media: Media) {
-    println!("{:#?}", media)
-}
-
-enum MightHaveAValue<'a> {
-    ThereIsAValue(&'a Media),
-    NoValueAvailable
-}
+mod content;
+use content::catalog::Catalog;
+use content::media::Media;
+// enum MightHaveAValue<'a> {
+//     ThereIsAValue(&'a Media),
+//     NoValueAvailable
+// }
 
 fn main() {
     let audiobook = Media::Audiobook {
