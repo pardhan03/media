aea1e5783c926b02185d52155cc72c27fb04d581
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-08-07 12:10:22 +0530

    implementation to enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+
+#[derive(Debug)]
+enum Media {
+    Book { title: String, author: String},
+    Movie { title: String, director: String},
+    Audiobook { title: String},
+}
+
+impl Media {
+    fn description(&self) -> String {
+        if let Media::Audiobook { title } = self {
+            format!("Audiobook: {}", title)
+        } else if let Media::Book {title, author} = self {
+            format!("Book {}, {}", title, author)
+        } else if let Media::Movie {title, director} = self {
+            format!("Movie: {}, {}", title, director)
+        } else {
+            String::from("Media description")
+        }
+    }
+}
+fn print_media(media: Media) {
+    println!("{:#?}", media)
+}
+
 fn main() {
+    let autiobook = Media::Audiobook { title: String::from("Awesome") };
+    let goog_movie = Media::Movie { title: String::from("Good Movie"), director: String::from("Good Director") };
+    let bad_book = Media::Book { title: String::from("Bad Book"), author: String::from("Bad Author") };
+
+    println!("{}",autiobook.description());
+    println!("{}",goog_movie.description());
+    println!("{}",bad_book.description());
+
     println!("Hello, world!");
 }
